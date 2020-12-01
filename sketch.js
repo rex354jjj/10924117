3f7399c4711bae803f21e0cd6eba6ad2a0e81d43
rex354jjj <55208420+rex354jjj@users.noreply.github.com>
2020-12-01 11:46:48 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://rex354jjj.github.io/10924117/model.json';
 
 
 function preload() {
